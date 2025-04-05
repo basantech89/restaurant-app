1bc673934e22edf322d9eacccd11fbe99d7cab43
Basant Soni <basant@myberrybox.com>
2025-04-05 19:36:33 +0530

    feat: add cognito user pool

---
+++ prettier.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleAttributePerLine: true,
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  printWidth: 80,
+  proseWrap: 'always',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  tabWidth: 2
+}
