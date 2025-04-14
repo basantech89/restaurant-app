a7a2ac24b4bfed0f37c55d2b2d6c12fb7eeac98f
Basant Soni <basant@myberrybox.com>
2025-04-14 09:50:53 +0530

    feat: load dynamic config

--- functions/get-restaurants.mjs
+++ functions/get-restaurants.mjs
@@ -1,29 +1,48 @@
+import middy from '@middy/core'
+import ssm from '@middy/ssm'
 import { DynamoDB } from '@aws-sdk/client-dynamodb'
 import { DynamoDBDocumentClient, ScanCommand } from '@aws-sdk/lib-dynamodb'
+
+import packageJson from '@aws-sdk/client-s3/package.json' with { type: 'json' }
+console.log('---------------- version', packageJson.version)
 
 const dynamodbClient = new DynamoDB()
 const dynamodb = DynamoDBDocumentClient.from(dynamodbClient)
 
-const defaultResults = parseInt(process.env.default_results)
 const tableName = process.env.restaurants_table
 
-const getRestaurants = async (count) => {
+const getRestaurants = async count => {
   console.log(`fetching ${count} restaurants from ${tableName}...`)
 
-  const resp = await dynamodb.send(new ScanCommand({
-    TableName: tableName,
-    Limit: count,
-  }))
+  const resp = await dynamodb.send(
+    new ScanCommand({
+      TableName: tableName,
+      Limit: count
+    })
+  )
   console.log(`found ${resp.Items.length} restaurants`)
   return resp.Items
 }
 
-export const handler = async (event, context) => {  
-  const restaurants = await getRestaurants(defaultResults)
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(restaurants)
-  }
+const { serviceName, stage } = process.env
 
-  return response
-}
+export const handler = middy()
+  .use(
+    ssm({
+      cache: true,
+      cacheExpiry: 1 * 60 * 1000, // 1 mins
+      setToContext: true,
+      fetchData: {
+        config: `/${serviceName}/${stage}/get-restaurants/config`
+      }
+    })
+  )
+  .handler(async (event, context) => {
+    const restaurants = await getRestaurants(context.config.defaultResults)
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(restaurants)
+    }
+
+    return response
+  })
